cbae87163c41f7ca0665380e7efe263a94853c71
Jonas Friedmann <j@frd.mn>
2015-08-05 11:03:20 +0200

    Improve /api/show tests

--- test/api.js
+++ test/api.js
@@ -8,13 +8,27 @@ var config = require('../vagrant-opt/config.mocha.json');
 describe('REST API', function() {
   describe('Route "GET /api/show"', function() {
     var uri = 'http://localhost:' + config.port + '/api/show';
-
-    it('should return status code 200', function(done) {
+    it('should return a payload that contains 2 hosts', function(done) {
       request({
         uri: uri,
         method: 'GET',
       }, function(error, response, body) {
-        expect(response.statusCode).to.equal(200);
+        var jsonBody = JSON.parse(body);
+        expect(jsonBody.payload.length).to.equal(2);
+        done();
+      });
+    });
+  });
+
+  describe('Route "GET /api/show/2"', function() {
+    var uri = 'http://localhost:' + config.port + '/api/show/2';
+    it('should return the hostname "reddit.com"', function(done) {
+      request({
+        uri: uri,
+        method: 'GET',
+      }, function(error, response, body) {
+        var jsonBody = JSON.parse(body);
+        expect(jsonBody.payload.host).to.equal("reddit.com");
         done();
       });
     });
